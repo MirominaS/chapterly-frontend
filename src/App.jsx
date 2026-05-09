b34bd134a5a62828d7041668a7c7171d54feaabd
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-05-09 23:55:11 +0530

    implemented react router dom routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
 import Button from './components/Button/Button'
+import Dashboard from './pages/Dashboard/Dashboard'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
+
 
 function App() {
   
   return (
-   <div>
-    <h1>Chapterly</h1>
-    <Button text="Test"/>
-   </div>
+    <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={}/> */}
+          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+        </Routes>
+    </BrowserRouter>
   )
+   
+  
 }
 
 export default App
