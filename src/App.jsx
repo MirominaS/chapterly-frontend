1d94adce71d281aa2c25133c898eb547f5131a5d
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-05-10 00:11:24 +0530

    created input component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import { BrowserRouter,Routes, Route } from 'react-router-dom'
-import Button from './components/Button/Button'
 import Dashboard from './pages/Dashboard/Dashboard'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
+import Input from './components/Input/Input'
 
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
   return (
     <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={}/> */}
+          <Route path="/" element={<Input placeholder={"Hello"}/>}/>
           <Route path="/dashboard" element={<Dashboard/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/register" element={<Register/>}/>
