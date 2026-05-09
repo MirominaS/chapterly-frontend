657189dccd9360256a27dedb6e7625a2464044f7
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-05-09 23:14:18 +0530

    installations and global styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Button from './components/Button/Button'
 
 function App() {
   
   return (
    <div>
     <h1>Chapterly</h1>
+    <Button/>
    </div>
   )
 }
