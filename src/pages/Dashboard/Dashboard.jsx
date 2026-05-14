7fc4e9d3e002a97a172d361d4bddd78df43ea624
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-05-14 13:29:53 +0530

    updated dashboard page

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 import './Dashboard.css'
+import MainLayout from '../../components/MainLayout/MainLayout'
+import StatCard from '../../components/StatCard/StatCard'
+import BookCard from '../../components/BookCard/BookCard'
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <MainLayout>
+      <div className='dashboard-container'>
+        <div className='dashboard-header'>
+          <h1>Dashboard</h1>
+          <p>Track your journey</p>
+        </div>
+        {/* stats */}
+        <div className='dashboard-stats'>
+          <StatCard title="Total Books" value="24"/>
+          <StatCard title="Completed" value="12"/>
+          <StatCard title="Currently Reading" value="5"/>
+          <StatCard title="Pages Read" value="8450"/>
+        </div>
+        {/* recent books */}
+        <div className='dashboard-section'>
+          <div className='dashboard-section-header'>
+            <h2>Recent Books</h2>
+          </div>
+          <div className='dashboard-books'>
+            <BookCard title="Pookkuzhi" author="Perumal murugan" progress="100" status="Completed"/>
+          </div>
+        </div>
+      </div>
+    </MainLayout>
   )
 }
 
