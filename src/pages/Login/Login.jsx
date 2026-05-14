ded312cbfcbdaf6f6e94b09325672ee6fd17e464
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-05-14 12:42:05 +0530

    created login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import './Login.css'
+import Input from '../../components/Input/Input'
+import Button from '../../components/Button/Button'
+import { Link } from 'react-router-dom'
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='login-container'>
+      <div className='login-card'>
+        <h1 className='login-title'>
+          Welcome Back
+        </h1>
+        <p className='login-subtitle'>
+          Continue your reading journey.
+        </p>
+        <div className='login-form'>
+          <Input label="Email" placeholder="Enter your email"/>
+          <Input label="Password" placeholder="Enter your password"/>
+          <Button text="Login"/>
+        </div>
+        <p className='login-footer'>
+          Don't have an account?
+          <Link to="/register">Register</Link>
+        </p>
+      </div>
+    </div>
   )
 }
 
