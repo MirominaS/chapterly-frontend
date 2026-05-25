85bd4e9f3b3786cbe67fee267a9b2aa0bff09fea
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-05-25 10:26:03 +0530

    add book fetched from backend

--- src/pages/AddBook/AddBook.jsx
+++ src/pages/AddBook/AddBook.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AddBook.css'
 import MainLayout from '../../components/MainLayout/MainLayout'
 import Input from '../../components/Input/Input'
 import Select from '../../components/Select/Select'
 import Textarea from '../../components/Textarea/Textarea'
 import Button from '../../components/Button/Button'
+import { createBook } from '../../services/bookService'
+import { useNavigate } from 'react-router-dom'
 
 const AddBook = () => {
+
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    title: '', author: '', genre: '', format: '', language: '',
+     total_pages: '', cover_image: '', description: ''
+  })
+  
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  const handleSubmit = async () => {
+    try {
+      await createBook(formData)
+      navigate('/books')
+    } catch (error) {
+      console.log(error)
+      alert(error.message)
+    }
+  }
+
   return (
     <MainLayout>
       <div className='add-book-container'>
@@ -19,16 +46,25 @@ const AddBook = () => {
             <Input
               label="Title"
               placeholder='Enter book title'
+              name="title"
+              value={formData.title}
+              onChange={handleChange}
             />
 
             <Input
               label="Author"
               placeholder='Enter author name'
+              name="author"
+              value={formData.author}
+              onChange={handleChange}
             />
 
             <Input
               label="Genre"
               placeholder='Fantasy, Self Help...'
+              name="genre"
+              value={formData.genre}
+              onChange={handleChange}
             />
 
             <Select
@@ -39,30 +75,45 @@ const AddBook = () => {
                 'Ebook',
                 'Audiobook',
               ]}
+              name="format"
+              value={formData.format}
+              onChange={handleChange}
             />
 
             <Input
               label="Language"
               placeholder='English'
+              name="language"
+              value={formData.language}
+              onChange={handleChange}
             />
 
             <Input
               label="Total Pages"
               placeholder='320'
               type='number'
+              name="total_pages"
+              value={formData.total_pages}
+              onChange={handleChange}
             />
 
             <Input
               label="Cover Image URL"
               placeholder='Paste image URL'
+              name="cover_image"
+              value={formData.cover_image}
+              onChange={handleChange}
             />
 
             <Textarea
               label="Description"
               placeholder='Write book description...'
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
             />
 
-            <Button text="Add Book" />
+            <Button text="Add Book" onClick={handleSubmit}/>
 
           </div>
         </div>
