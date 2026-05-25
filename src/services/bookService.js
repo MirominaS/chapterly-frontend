e368b0b285c0d6739c0b4956aec29d31ae92dd14
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-05-25 10:27:14 +0530

    get books and create book services implemented to fetch from backend

---
+++ src/services/bookService.js
@@ -0,0 +1,43 @@
+const API_URL = import.meta.env.VITE_API_URL 
+
+export const getBooks = async () => {
+    const token = localStorage.getItem('token')
+
+    const response = await fetch(
+        `${API_URL}/books`,
+        {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+    )
+    const data = await response.json()
+
+    if(!response.ok) {
+        throw new Error(data.message)
+    }
+    return data
+}
+
+export const createBook =  async (bookData) => {
+    const token = localStorage.getItem('token')
+
+    const response = await fetch(
+        `${API_URL}/books`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json',
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify(bookData)
+        }
+    )
+    const data = await response.json()
+
+    if(!response.ok) {
+        throw new Error(data.message)
+    }
+
+    return data
+}
