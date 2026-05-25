cb6a4fdbc27f55c9e5d8db9868a40f9dedebdb4a
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-05-25 10:27:33 +0530

    get books fetched

--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -1,42 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Books.css'
 import MainLayout from '../../components/MainLayout/MainLayout'
 import Button from '../../components/Button/Button'
 import Input from '../../components/Input/Input'
 import Select from '../../components/Select/Select'
 import BookCard from '../../components/BookCard/BookCard'
+import { getBooks } from '../../services/bookService'
+import Loader from '../../components/Loader/Loader'
+import { useNavigate } from 'react-router-dom'
 
 const Books = () => {
 
-    const books = [
-    {
-      id:1,
-      title:'Atomic Habits',
-    },
-    {
-      id:2,
-      title:'Deep Work',
-    },
-    {
-      id:3,
-      title:'The Alchemist',
-    },
-      {
-      id:1,
-      title:'Atomic Habits',
-    },
-    {
-      id:2,
-      title:'Deep Work',
-    },
-    {
-      id:3,
-      title:'The Alchemist',
-    },
-  ]
+  const navigate = useNavigate();
 
+  const [books, setBooks] = useState([])
   const [statusFilter,setStatusFilter] = useState('');
   const [langFilter, setLangFilter] = useState('');
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+        try{
+            const data = await getBooks()
+            setBooks(data)
+        } catch(error) {
+            console.log(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+    fetchBooks()
+  },[])
+
+  if(loading) {
+    return <Loader/>
+  }
 
   return (
     <MainLayout>
@@ -46,7 +44,7 @@ const Books = () => {
                     <h1 className='books-title'>My Books</h1>
                     <p className='books-subtitle'>Manage your reading collection</p>
                 </div>
-                <Button text="Add Book"/>
+                <Button text="Add Book" onClick={() => navigate('/add-book')}/>
             </div>
             <div className='books-controls'>
                 <Input label="Search books" placeholder="Search books..."/>
@@ -74,6 +72,9 @@ const Books = () => {
                         <BookCard 
                             key={book.id}
                             title={book.title}
+                            author={book.author}
+                            progress={book.progress_percentage}
+                            status={book.status}
                         />
                     ))
                 }
